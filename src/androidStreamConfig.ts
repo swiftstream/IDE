1904df4b3223995adf66bf492e3f4b9f0e707651
Mikhail Isaev <isaev.mihael@gmail.com>
2025-09-30 12:18:46 +0400

    🛠️ Android: change supported API range to 28-36

--- src/androidStreamConfig.ts
+++ src/androidStreamConfig.ts
@@ -73,7 +73,7 @@ export class AndroidStreamConfig {
 
     static async askForMinSDK(): Promise<string | undefined> {
         return await window.showQuickPick([
-            '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35'
+            '28', '29', '30', '31', '32', '33', '34', '35', '36'
         ], {
             title: 'Android Min SDK Version',
             placeHolder: `Choose Android Min SDK Version`
@@ -82,7 +82,7 @@ export class AndroidStreamConfig {
 
     static async askForCompileSDK(): Promise<string | undefined> {
         return await window.showQuickPick([
-            '35', '34', '33', '32', '31', '30', '29', '28', '27', '26', '25', '24'
+            '36', '35', '34', '33', '32', '31', '30', '29', '28'
         ], {
             title: 'Android Compile SDK Version',
             placeHolder: `Choose Android Compile SDK Version`
